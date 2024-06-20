94bd0951190787a33ca66b90279e0a31136c300d
Jiye-Park01 <jiye0710@gmail.com>
2024-06-20 20:15:45 +0900

    학습 확인 + 테스트  완료

---
+++ main2.py
@@ -0,0 +1,312 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+import re
+import json
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+
+DATA_PATH = './DATA/' #데이터경로 설정
+print('파일 크기: ')
+for file in os.listdir(DATA_PATH):
+  if 'txt' in file:
+    print(file.ljust(30)+str(round(os.path.getsize(DATA_PATH+ file) / 100000,2))+'MB')
+    #파일크기
+
+
+#학습데이터 불러오기
+train_data = pd.read_csv(DATA_PATH + 'ratings_train.txt',header = 0, delimiter = '\t', quoting=3)
+train_data.head()
+
+#학습데이터갯수
+print('학습데이터 전체 개수: {}'.format(len(train_data)))
+
+
+#리뷰 전체길이 확인
+train_length = train_data['document'].astype(str).apply(len)
+train_length.head()
+
+#리뷰 통계 정보
+print('리뷰 길이 최댓값: {}'.format(np.max(train_length)))
+print('리뷰 길이 최솟값: {}'.format(np.min(train_length)))
+print('리뷰 길이 평균값: {:.2f}'.format(np.mean(train_length)))
+print('리뷰 길이 표준편차: {:.2f}'.format(np.std(train_length)))
+print('리뷰 길이 중간값: {}'.format(np.median(train_length)))
+print('리뷰 길이 제1사분위: {}'.format(np.percentile(train_length,25)))
+print('리뷰 길이 제3사분위: {}'.format(np.percentile(train_length,75)))
+
+
+# 문자열 아닌 데이터 모두 제거
+train_review = [review for review in train_data['document'] if type(review) is str]
+train_review
+
+
+# 한글 폰트 설정(.ttf파일 다운로드 후 실행)
+font_path = r'C:/jiye/JP_emotion/cmb10.ttf'
+
+# train_review 리스트에 있는 모든 리뷰를 공백으로 구분하여 한 번에 WordCloud 생성
+wordcloud = WordCloud(font_path=font_path).generate(' '.join(train_review))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+
+
+#긍정 1, 부정 0
+print('긍정 리뷰 갯수: {}'.format(train_data['label'].value_counts()[1]))
+print('부정 리뷰 갯수: {}'.format(train_data['label'].value_counts()[0]))
+
+#[전처리]
+# 1. 전처리 함수 만들기
+def preprocessing(review, okt, remove_stopwords=False, stop_words=[]):
+  # 함수인자설명
+  # review: 전처리할 텍스트
+  # okt: okt객체를 반복적으로 생성하지 않고 미리 생성 후 인자로 받음
+  # remove_stopword: 불용어를 제거할지 여부 선택. 기본값 False
+  # stop_words: 불용어 사전은 사용자가 직접 입력, 기본값 빈 리스트
+
+  # 한글 및 공백 제외한 문자 모두 제거
+    review_text = re.sub('[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]', '', review)
+
+    # okt 객체를 활용하여 형태소 단어로 나눔
+    word_review = okt.morphs(review_text, stem=True)
+
+    if remove_stopwords:
+      # 불용어 제거(선택)
+      word_review = [token for token in word_review if token not in stop_words]
+    return word_review
+
+
+# 2. 전체 학습데이터 및 평가 데이터 리뷰 전처리하기
+#전체 텍스트 전처리
+stop_words = ['은','는','이','가','하','아','것','들','의','있','되','수','보','주','등','한']
+okt = Okt()
+clean_train_review = []
+
+for review in train_data['document']:
+  # 리뷰가 문자열인 경우만 전처리 진행
+  if type(review) == str:
+    clean_train_review.append(preprocessing(review,okt,remove_stopwords=True,stop_words= stop_words))
+  else:
+    clean_train_review.append([]) #str이 아닌 행은 빈칸으로 놔두기
+
+clean_train_review[:4]
+
+
+#테스트 리뷰도 동일하게 전처리
+test_data = pd.read_csv(DATA_PATH + 'ratings_test.txt', header = 0, delimiter='\t', quoting=3)
+
+clean_test_review = []
+for review in test_data['document']:
+  if type(review) == str:
+    clean_test_review.append(preprocessing(review, okt, remove_stopwords=True, stop_words=stop_words))
+  else:
+    clean_test_review.append([])
+
+
+#3. 문자로 되어있는 리뷰데이터를 인덱스 벡터로 변환
+# 인덱스 벡터 변환 후 일정 길이 넘어가거나 모자라는 리뷰 패딩처리
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(clean_train_review)
+train_sequences = tokenizer.texts_to_sequences(clean_train_review)
+test_sequences = tokenizer.texts_to_sequences(clean_test_review)
+
+word_vocab = tokenizer.word_index #단어사전형태
+MAX_SEQUENCE_LENGTH = 8 #문장 최대 길이
+
+#학습 데이터
+train_inputs = pad_sequences(train_sequences,maxlen=MAX_SEQUENCE_LENGTH,padding='post')
+
+#학습 데이터 라벨 벡터화
+train_labels = np.array(train_data['label'])
+
+#평가 데이터
+test_inputs = pad_sequences(test_sequences,maxlen=MAX_SEQUENCE_LENGTH,padding='post')
+#평가 데이터 라벨 벡터화
+test_labels = np.array(test_data['label'])
+
+
+#4. 전처리 완료된 데이터 넘파이 파일로 저장
+DEFAULT_PATH  = '/content/sample_data/' # 경로지정
+DATA_PATH = './CLEAN_DATA/' #.npy파일 저장 경로지정
+TRAIN_INPUT_DATA = 'nsmc_train_input.npy'
+TRAIN_LABEL_DATA = 'nsmc_train_label.npy'
+TEST_INPUT_DATA = 'nsmc_test_input.npy'
+TEST_LABEL_DATA = 'nsmc_test_label.npy'
+DATA_CONFIGS = 'data_configs.json'
+
+data_configs={}
+data_configs['vocab'] = word_vocab
+data_configs['vocab_size'] = len(word_vocab) + 1
+
+#전처리한 데이터들 파일로저장
+import os
+
+if not os.path.exists(DEFAULT_PATH + DATA_PATH):
+  os.makedirs(DEFAULT_PATH+DATA_PATH)
+
+#전처리 학습데이터 넘파이로 저장
+np.save(open(DEFAULT_PATH+DATA_PATH+TRAIN_INPUT_DATA,'wb'),train_inputs)
+np.save(open(DEFAULT_PATH+DATA_PATH+TRAIN_LABEL_DATA,'wb'),train_labels)
+#전처리 테스트데이터 넘파이로 저장
+np.save(open(DEFAULT_PATH+DATA_PATH+TEST_INPUT_DATA,'wb'),test_inputs)
+np.save(open(DEFAULT_PATH+DATA_PATH+TEST_LABEL_DATA,'wb'),test_labels)
+
+#데이터 사전 json으로 저장
+json.dump(data_configs,open(DEFAULT_PATH + DATA_PATH + DATA_CONFIGS,'w'),ensure_ascii=False)
+
+
+
+#모델 학습하기
+# 학습 데이터 불러오기
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import json
+from tqdm import tqdm
+
+#전처리 데이터 불러오기
+DATA_PATH = '/content/sample_data/CLEAN_DATA/'
+DATA_OUT = '/content/sample_data/DATA_OUT/'
+INPUT_TRAIN_DATA = 'nsmc_train_input.npy'
+LABEL_TRAIN_DATA = 'nsmc_train_label.npy'
+DATA_CONFIGS = 'data_configs.json'
+
+train_input = np.load(open(DATA_PATH + INPUT_TRAIN_DATA,'rb'))
+train_input = pad_sequences(train_input,maxlen=train_input.shape[1])
+train_label = np.load(open(DATA_PATH + LABEL_TRAIN_DATA,'rb'))
+prepro_configs = json.load(open(DATA_PATH+DATA_CONFIGS,'r'))
+
+
+
+
+#파라미터 세팅하기
+model_name= 'cnn_classifier_kr'
+BATCH_SIZE = 512
+NUM_EPOCHS = 10
+VALID_SPLIT = 0.1
+MAX_LEN = train_input.shape[1]
+
+kargs={'model_name': model_name, 'vocab_size':prepro_configs['vocab_size'],'embbeding_size':128, 'num_filters':100,'dropout_rate':0.5, 'hidden_dimension':250,'output_dimension':1}
+
+
+
+#모델 함수 만들기
+class CNNClassifier(tf.keras.Model):
+
+  def __init__(self, **kargs):
+    super(CNNClassifier, self).__init__(name=kargs['model_name'])
+    self.embedding = layers.Embedding(input_dim=kargs['vocab_size'], output_dim=kargs['embbeding_size'])
+    self.conv_list = [layers.Conv1D(filters=kargs['num_filters'], kernel_size=kernel_size, padding='valid',
+                                    activation=tf.keras.activations.relu,
+                                    kernel_constraint=tf.keras.constraints.MaxNorm(max_value=3)) for kernel_size in
+                      [3, 4, 5]]
+    self.pooling = layers.GlobalMaxPooling1D()
+    self.dropout = layers.Dropout(kargs['dropout_rate'])
+    self.fc1 = layers.Dense(units=kargs['hidden_dimension'],
+                            activation=tf.keras.activations.relu,
+                            kernel_constraint=tf.keras.constraints.MaxNorm(max_value=3.))
+    self.fc2 = layers.Dense(units=kargs['output_dimension'],
+                            activation=tf.keras.activations.sigmoid,
+                            kernel_constraint=tf.keras.constraints.MaxNorm(max_value=3.))
+
+  def call(self, x):
+    x = self.embedding(x)
+    x = self.dropout(x)
+    x = tf.concat([self.pooling(conv(x)) for conv in self.conv_list], axis=1)
+    x = self.fc1(x)
+    x = self.fc2(x)
+    return x
+
+
+
+#학습하기
+from tensorflow.keras.models import save_model
+
+model = CNNClassifier(**kargs)
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss = tf.keras.losses.BinaryCrossentropy(),
+              metrics = [tf.keras.metrics.BinaryAccuracy(name='accuracy')])
+
+#검증 정확도를 통한 EarlyStopping 기능 및 모델 저장 방식 지정
+earlystop_callback = EarlyStopping(monitor='val_accuracy', min_delta=0.0001, patience=2)
+checkpoint_path = DATA_OUT + model_name +'\weights.h5'
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+if os.path.exists(checkpoint_dir):
+  print("{} -- Folder already exists \n".format(checkpoint_dir))
+else:
+  os.makedirs(checkpoint_dir, exist_ok=True)
+  print("{} -- Folder create complete \n".format(checkpoint_dir))
+
+cp_callback = ModelCheckpoint(
+    checkpoint_path, monitor = 'val_accuracy', verbose=1, save_best_only = True,
+    save_weights_only=True
+)
+
+history = model.fit(train_input, train_label, batch_size=BATCH_SIZE, epochs = NUM_EPOCHS,
+                    validation_split=VALID_SPLIT, callbacks=[earlystop_callback, cp_callback])
+# 모델 저장하기
+save_model(model,'/content/sample_data/my_models/')
+
+
+
+#평가하기
+INPUT_TEST_DATA = 'nsmc_test_input.npy'
+LABEL_TEST_DATA = 'nsmc_test_label.npy'
+SAVE_FILE_NM = 'weights.h5'
+
+test_input = np.load(open(DATA_PATH+INPUT_TEST_DATA,'rb'))
+test_input = pad_sequences(test_input,maxlen=test_input.shape[1])
+test_label_data = np.load(open(DATA_PATH + LABEL_TEST_DATA, 'rb'))
+
+model.load_weights('/content/sample_data/DATA_OUT/cnn_classifier_kr/weights.h5')
+model.evaluate(test_input, test_label_data)
+
+
+
+#예측하기
+import numpy as np
+import pandas as pd
+import re
+import json
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+okt = Okt()
+tokenizer  = Tokenizer()
+
+DATA_CONFIGS = 'data_configs.json'
+prepro_configs = json.load(open('/content/sample_data/CLEAN_DATA/'+DATA_CONFIGS,'r'))
+prepro_configs['vocab'] = word_vocab
+
+tokenizer.fit_on_texts(word_vocab)
+
+MAX_LENGTH = 8 #문장최대길이
+
+sentence = input('감성분석할 문장을 입력해 주세요.: ')
+sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣\\s ]','', sentence)
+stopwords = ['은','는','이','가','하','아','것','들','의','있','되','수','보','주','등','한'] # 불용어 추가할 것이 있으면 이곳에 추가
+sentence = okt.morphs(sentence, stem=True) # 토큰화
+sentence = [word for word in sentence if not word in stopwords] # 불용어 제거
+vector  = tokenizer.texts_to_sequences(sentence)
+pad_new = pad_sequences(vector, maxlen = MAX_LENGTH) # 패딩
+
+model.load_weights('/content/sample_data/DATA_OUT/cnn_classifier_kr\weights.h5') #모델 불러오기
+predictions = model.predict(pad_new)
+predictions = float(predictions.squeeze(-1)[1])
+
+if(predictions > 0.5):
+  print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(predictions * 100))
+else:
+  print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - predictions) * 100))
